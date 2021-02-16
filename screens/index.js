08b65f44ee1073a4d90fb7345cac32d3c1e7d713
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-02-16 11:19:38 -0300

    Desenvolvendo tela de cadastro de atividade

--- screens/index.js
+++ screens/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Screen from './screen'
 import Inicio from '../components/Inicio'
+import Cadastro from '../components/Cadastro'
 
 export const indexScreen = ({navigation}) => 
     <Screen navigation={navigation} name="index">
         <Inicio />
     </Screen>
 
-export const cadastroScreen = ({navigation}) => <Screen navigation={navigation} name="Cadastro" />
+export const cadastroScreen = ({navigation}) => 
+    <Screen navigation={navigation} name="Cadastro">
+        <Cadastro />
+    </Screen>
 
 export const cardScreen = ({navigation}) => <Screen navigation={navigation} name="Card" />
 
